281ef386354ca884b2e9e4de237579740cc9fde6
Pantelis Fanourgakis <pantelis.fanourgakis@gmail.com>
2019-04-22 23:21:47 +0100

    trying to implement unit testing

--- spec/numerical.spec.js
+++ spec/numerical.spec.js
@@ -1,21 +1,58 @@
-const controller = require('../controllers.js')
-
+const controller = require('../src/controllers.js')
 
 describe('Testing numerical conversions', () => {
-  it('Testing error case: False roman input ', () => {
-    const message = controller.romanToArab('/roman/ABC');
-    expect(message).toBe(getErrorMessage('Not a valid roman number'));
+  //Error cases
+  it('Testing error case: Roman to Arabic invalid characters "QJO"', async () => {
+    let message;
+    try {
+      message = await controller.romanToArab('QJO')
+    } catch (error) {
+      // expect(error).toBe(400)
+      message = error;
+    }
+    expect(message).toBe(400);
+  });
+
+
+  it('Testing error case: Arabic to Roman invalid input "ABC"', async () => {
+    let message;
+    try {
+      message = await controller.arabToRoman('ABC')
+    } catch (error) {
+      message = error
+    }
+    expect(message).toBe(400);
   })
-  it('Testing error case: False arabic input', () => {
-    const message = controller.arabToRoman('/arabic/ABC');
-    expect(message).toBe(getErrorMessage('Not a valid arabic number'));
+
+  it('Testing error case: GET all with invalid parameter', async () => {
+    let message;
+    try {
+      message = await controller.arabToRoman('/all/sumerian');
+
+    } catch (error) {
+      message = error
+    }
+    expect(message).toBe(400);
   })
-  it('Testing case: Correct arabic number', () => {
-    const message = controller.arabToRoman('/arabic/12');
-    expect(message).toBe(getErrorMessage('{ inputValue: 12, convertedValue: XII }'));
+
+
+  //Valid cases
+  it('Testing case: Valid Roman characters to Arabic number "XII"', async () => {
+    let message;
+    try {
+      message = await controller.romanToArab('XII')
+    } catch (error) {
+      message = error
+    }
+    expect(message.convertedValue).toBe(12)
   })
-  it('Testing case: Correct roman number', () => {
-    const message = controller.romanToArab('/roman/XII');
-    expect(message).toBe(getErrorMessage('{ inputValue: XII, convertedValue: 12 }'));
+  it('Testing case: Valid Arabic number "12" to Roman Characters "XII"', async () => {
+    let message;
+    try {
+      message = await controller.arabToRoman(12)
+    } catch (error) {
+      message = error
+    }
+    expect(message.convertedValue).toBe("XII")
   })
 })
